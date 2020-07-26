d4bf7dd56d02e051f122f6aae57cd6a33c2892e8
nick fisher <fisher.nick14@gmail.com>
2020-07-26 12:34:11 -0700

    refactor

--- context-api/src/main/java/com/nickolasfisher/webfluxcontext/HelloController.java
+++ context-api/src/main/java/com/nickolasfisher/webfluxcontext/HelloController.java
@@ -2,14 +2,9 @@ package com.nickolasfisher.webfluxcontext;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import java.net.URI;
-import java.util.function.Function;
 
 import static com.nickolasfisher.webfluxcontext.WebContextFilter.X_CUSTOM_HEADER;
 
@@ -22,17 +17,17 @@ public class HelloController {
         this.webClient = webClient;
     }
 
-    @GetMapping("/hello/{name}")
-    public Mono<ResponseEntity<String>> hello(@PathVariable("name") String name) {
+    @GetMapping("/hello")
+    public Mono<ResponseEntity<String>> hello() {
         return Mono.subscriberContext()
-                .flatMap(context -> {
-                    return webClient.get()
-                            .uri("/test")
-                            .exchange()
-                            .map(clientResponse -> {
-                                String[] strings = context.get(X_CUSTOM_HEADER);
-                                return ResponseEntity.status(200).header(X_CUSTOM_HEADER, strings).build();
-                            });
-                });
+                .flatMap(context -> webClient.get()
+                        .uri("/test")
+                        .exchange()
+                        .map(clientResponse -> {
+                            String[] strings = context.get(X_CUSTOM_HEADER);
+                            return ResponseEntity.status(200)
+                                    .header(X_CUSTOM_HEADER, strings)
+                                    .build();
+                        }));
     }
 }
