32c07aa2064c9c1088e0532dc51aed4cb01036c9
nick fisher <fisher.nick14@gmail.com>
2021-04-03 17:21:35 -0700

    test container test example

---
+++ reactive-redis/src/test/java/com/nickolasfisher/reactiveredis/RedisTestContainerTest.java
@@ -0,0 +1,45 @@
+package com.nickolasfisher.reactiveredis;
+
+import io.lettuce.core.RedisClient;
+import org.junit.ClassRule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@Testcontainers
+public class RedisTestContainerTest {
+
+    @Container
+    public static GenericContainer genericContainer = new GenericContainer(
+                DockerImageName.parse("redis:5.0.3-alpine")
+            ).withExposedPorts(6379);
+
+    private RedisDataService redisDataService;
+
+    @BeforeEach
+    public void setupRedisClient() {
+        RedisClient redisClient = RedisClient.create("redis://" + genericContainer.getHost() + ":" + genericContainer.getMappedPort(6379));
+        redisDataService = new RedisDataService(redisClient.connect().reactive());
+    }
+
+    @Test
+    public void canWriteAndReadThing() {
+        Mono<Void> writeMono = redisDataService.writeThing(Thing.builder().id(1).value("hello-redis").build());
+
+        StepVerifier.create(writeMono).verifyComplete();
+
+        StepVerifier.create(redisDataService.getThing(1))
+                .expectNextMatches(thing ->
+                        thing.getId() == 1 &&
+                                "hello-redis".equals(thing.getValue())
+                )
+                .verifyComplete();
+    }
+}
